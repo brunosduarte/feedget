5c9b393e3e3741a8f92bdc2e7392e300760a3092
Bruno FURG <brunosduarte@furg.br>
2022-05-07 15:55:11 -0300

    stage5-4

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,21 +7,26 @@ export const routes = express.Router()
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
+
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
     
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
-
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-        )
-
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    })
-
-    return res.status(201).send();
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+            )
+    
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot,
+        })
+ 
+        return res.status(201).send();
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send();
+    }
 });
